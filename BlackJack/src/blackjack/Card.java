4e4e6b4a8b59542bcf8f6c1596f593308e102647
ZachTom2 <2005988@student.acalanes.k12.ca.us>
2017-03-14 13:07:31 -0700

    Done Stuff

---
+++ BlackJack/src/blackjack/Card.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blackjack;
+
+/**
+ *
+ * @author 2005988
+ */
+public class Card {
+    public String type;
+	public String suit;
+	public int value;
+	public int[] ace = new int[2]; 
+	public Card(String t, String s, int val){
+		type = t;
+		suit = s;
+		value = val;
+	}
+	public Card(String t, String s, int[] val){
+		type = t;
+		suit = s;
+		for(int i = 0; i<val.length; i++){
+			ace[i] = val[i];
+		}
+	}
+    @Override
+	public String toString() {
+		return type + " of " + suit + " which has a point value of" + value;
+	}
+}
