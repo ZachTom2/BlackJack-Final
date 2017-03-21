477a6c8d75a8a96a8bad5a976af22f6298713023
ZachTom2 <2005988@student.acalanes.k12.ca.us>
2017-03-21 13:07:15 -0700

    Done for today

--- BlackJack/src/blackjack/PlayBlackJack.java
+++ BlackJack/src/blackjack/PlayBlackJack.java
@@ -48,6 +48,9 @@ public PlayBlackJack(int decks){
         Card deal2 = temp.removeCard(0);
         deal.add(deal1);
         deal.add(deal2);
+        System.out.print("The dealer has showing");
+        deal.show(deal1);
+        
         
         
     }
@@ -56,6 +59,51 @@ public PlayBlackJack(int decks){
     }
     
     
-    
+    public void playerChoice(int choice){
+        switch(choice)
+            {
+            case 0:
+                System.out.println("Bye!");
+                break;
+            case 1:
+                 System.out.println
+                break;
+            case 2:
+         
+                break;
+            case 3:
+              
+                break;
+            case 4:
+               
+                break;
+             
+            case 5:
+                break;
+            case 6:
+            
+                break;
+            case 7:
+             
+            break;
+             
+            default:
+                System.out.println("Sorry, invalid choice");
+            }
+    }
+    public static void printMenu()
+    {
+        System.out.println("\n   Menu   ");
+        System.out.println("   ====");
+        System.out.println("0: Quit");
+        System.out.println("1: Stand)");
+        System.out.println("2: Hit");
+        System.out.println("3: ");
+        System.out.println("4: ");
+        System.out.println("5: ");
+        System.out.println("6: ");
+        System.out.println("7: ");
+        System.out.print("\nEnter your choice: ");
+    }
     
 }
